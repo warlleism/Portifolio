b14f7f5c233f32925a33b54a6201178504c61638
Warllei Martins <warllei.s.martins@hotmail.com>
2022-08-03 00:47:55 -0300

    first commit

---
+++ src/view/body/index.jsx
@@ -0,0 +1,85 @@
+import React, { useEffect } from "react";
+import "./style.scss"
+import Png1 from "../../image/png1.png"
+import Png2 from "../../image/png2.png"
+import Png3 from "../../image/png3.png"
+import Png4 from "../../image/png4.png"
+
+
+const Body = () => {
+
+    useEffect(() => {
+        // window.scrollTo(0, 0)
+        showAbout()
+    }, [])
+
+    const showAbout = () => {
+        const about = window.document.getElementById("about")
+        const descApp = window.document.getElementById("desc-app")
+        const descSecondApp = window.document.getElementById("desc-second-app")
+        window.addEventListener("scroll", () => {
+            console.log(window.scrollY)
+            window.scrollY <= 0 ? about.style.transform = "translateX(-100%)" : about.style.transform = "translateX(5%)"
+            window.scrollY <= 1000 ? descApp.style.transform = "translateX(-100%)" : descApp.style.transform = "translateX(0%)"
+            window.scrollY <= 2500 ? descSecondApp.style.transform = "translateX(-100%)" : descSecondApp.style.transform = "translateX(0%)"
+        })
+    }
+
+
+    return (
+        <div className="main-body">
+            <div className="content-desc">
+                <div className="about" id="about">SOBRE MIM</div>
+                <div className="desc">Desenvolvedor Front-end completamente apaixonado pela programação. Com profundo interesse em novas tecnologias.</div>
+            </div>
+
+            <div className="app-content">
+                <div id="desc-app" className="desc-app">
+                    <div>
+                        <div className="linguage">React Native</div>
+                        <div className="project">Nubank</div>
+                        <div className="desc-projeto">Nubank é uma empresa startup brasileira pioneira no segmento de serviços financeiros, atuando como operadora de cartões de crédito e fintech com operações no Brasil, sediada em São Paulo e fundada em 6 de maio de 2013 por David Vélez, Cristina Junqueira e Edward Wible. </div>
+                    </div>
+                    <div className="system">Android APP</div>
+                </div>
+
+                <div className="image-app">
+                    <div>
+                        <img className="bottonMarginPhoto" src={Png1} alt="" />
+                        <img src={Png2} alt="" />
+                    </div>
+                    <div>
+                        <img className="bottonMarginPhoto" src={Png3} alt="" />
+                        <img src={Png4} alt="" />
+                    </div>
+                </div>
+            </div>
+
+            <div className="app-content">
+                <div id="desc-second-app" className="desc-app">
+                    <div>
+                        <div className="linguage">React Native</div>
+                        <div className="project">Nubank</div>
+                        <div className="desc-projeto">Nubank é uma empresa startup brasileira pioneira no segmento de serviços financeiros, atuando como operadora de cartões de crédito e fintech com operações no Brasil, sediada em São Paulo e fundada em 6 de maio de 2013 por David Vélez, Cristina Junqueira e Edward Wible. </div>
+                    </div>
+                    <div className="system">Android APP</div>
+                </div>
+
+                <div className="image-app">
+                    <div>
+                        <img className="bottonMarginPhoto" src={Png1} alt="" />
+                        <img src={Png2} alt="" />
+                    </div>
+                    <div>
+                        <img className="bottonMarginPhoto" src={Png3} alt="" />
+                        <img src={Png4} alt="" />
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    )
+
+}
+
+export default Body;
---
+++ src/view/header/index.jsx
@@ -0,0 +1,41 @@
+import React, { useEffect } from "react";
+import "./style.scss"
+
+
+const Header = () => {
+
+    useEffect(() => {
+        window.scrollTo(0, 0)
+        showContent()
+    }, [])
+
+    const showContent = () => {
+        let name = window.document.getElementById("name")
+        let desc = window.document.getElementById("desc")
+        window.addEventListener("load", () => {
+            name.style.transform = "translateY(-130px)";
+            setTimeout(() => {
+                desc.style.transform = "translateY(-130px)";
+            }, 900)
+        })
+    }
+
+
+    return (
+        <div className="main-header">
+            <div className="header-content">
+                <div className="name-content">
+                    <div className="text" id="name">Warllei Martins</div>
+                    <div className="board" ></div>
+                </div>
+                <div className="cargo-content">
+                    <div className="text" id="desc">Front-end Developer </div>
+                    <div className="board" ></div>
+                </div>
+            </div>
+        </div>
+    )
+
+}
+
+export default Header;
