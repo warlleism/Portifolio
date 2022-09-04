e9c3a711819a2c1287f01547e404257a4def8f33
Warllei Martins <warllei.s.martins@hotmail.com>
2022-09-04 14:07:27 -0300

    build de produção

--- src/context/provider.js
+++ src/context/provider.js
@@ -4,7 +4,7 @@ export const Context = createContext()
 
 export default function Provider({ children }) {
 
-    const [mode, setMode] = useState(localStorage.getItem("mode") == "Light" ? true : false)
+    const [mode, setMode] = useState(localStorage.getItem("mode") == "Light" ? true :false)
 
     return (
         <Context.Provider value={{ mode, setMode }}>
