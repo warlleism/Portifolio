5efd36bd9655424cd64753e708d851f0563439b4
Warllei Martins <warllei.s.martins@hotmail.com>
2023-01-08 21:52:15 -0300

    mais conteúdo

--- src/view/body/index.jsx
+++ src/view/body/index.jsx
@@ -5,22 +5,12 @@ import { Context } from "../../context/provider";
 import "./style.scss";
 
 const Body = () => {
-  useEffect(() => {
-    showAbout();
-  }, []);
+
+
   const [img, setImg] = useState('')
   const [imgwEB, setImgWeb] = useState('')
 
   const { mode } = useContext(Context);
-
-  const showAbout = () => {
-    const about = window.document.getElementById("about"); const descApp = window.document.getElementById("desc-app"); const descSecondApp = window.document.getElementById("desc-second-app"); const descThirdApp = window.document.getElementById("desc-third-app"); const descFourthApp = window.document.getElementById("desc-fourth-app"); const descfifthApp = window.document.getElementById("desc-fifth-app"); const descSixthApp = window.document.getElementById("desc-sixth-app"); const descSeventhApp = window.document.getElementById("desc-seventh-app"); const descOctaveApp = window.document.getElementById("desc-octave-app"); const descNinthApp = window.document.getElementById("desc-ninth-app"); const descTenthApp = window.document.getElementById("desc-tenth-app");
-
-    window.addEventListener("scroll", () => {
-      const windowScroll = parseInt(window.scrollY);
-      windowScroll <= 13900 ? (descNinthApp.style.transform = "translateX(-100%)") : (descNinthApp.style.transform = "translateX(0%)"); windowScroll <= 16000 ? (descTenthApp.style.transform = "translateX(-100%)") : (descTenthApp.style.transform = "translateX(0%)"); windowScroll <= 0 ? (about.style.transform = "translateX(-100%)") : (about.style.transform = "translateX(5%)"); windowScroll <= 600 ? (descApp.style.transform = "translateX(-100%)") : (descApp.style.transform = "translateX(0%)"); windowScroll <= 3025 ? (descSecondApp.style.transform = "translateX(-100%)") : (descSecondApp.style.transform = "translateX(0%)"); windowScroll <= 5143 ? (descThirdApp.style.transform = "translateX(-100%)") : (descThirdApp.style.transform = "translateX(0%)"); windowScroll <= 6909 ? (descFourthApp.style.transform = "translateX(-100%)") : (descFourthApp.style.transform = "translateX(0%)"); windowScroll <= 8518 ? (descfifthApp.style.transform = "translateX(-100%)") : (descfifthApp.style.transform = "translateX(0%)"); windowScroll <= 10305 ? (descSixthApp.style.transform = "translateX(-100%)") : (descSixthApp.style.transform = "translateX(0%)"); windowScroll <= 11800 ? (descSeventhApp.style.transform = "translateX(-100%)") : (descSeventhApp.style.transform = "translateX(0%)"); windowScroll <= 12900 ? (descOctaveApp.style.transform = "translateX(-100%)") : (descOctaveApp.style.transform = "translateX(0%)");
-    })
-  }
 
   const ViewContentMobile = (require) => {
 
@@ -98,10 +88,10 @@ const Body = () => {
             </strong>
             ! Meu nome é Warllei Martins, espero que esteja tudo bem com você.
             Tenho 26 anos e sou estudante de Análise e desenvolvimento de
-            sistemas pela "Universidade de Vila Velha". Sou um desenvolvedor
-            front-end em busca de me tornar full stack. Com profundo interesse
-            em conhecer, aprender e desenvolver venho me dedicado a este
-            apaixonante mundo da programação.
+            sistemas pela "Universidade Vila Velha". Sou um desenvolvedor
+            front-end em busca de me tornar full-stack. Com profundo interesse
+            em conhecer, aprender e desenvolver. venho me dedicado ao mundo da técnologia
+            desde abril de 2021.
           </div>
         </div>
 
@@ -404,10 +394,11 @@ const Body = () => {
           <div id="desc-seventh-app" className="desc-app">
             <div>
               <div className="linguage" style={{ color: mode ? "" : "#ffff" }}>
-                Figma
+                ReactJs
               </div>
               <div className="project" style={{ color: mode ? "" : "#ffff" }}>
-                Projetos de designer
+                A Nissan é uma fabricante japonesa de automóveis resultante de uma fusão entre Datsun e Prince.
+                A Nissan está listada no Nikkei 225 e é a terceira maior fabricante japonesa de automóveis depois da Toyota e Honda em capitalização de mercado.
               </div>
               <div
                 className="desc-projeto"
@@ -417,7 +408,7 @@ const Body = () => {
               </div>
             </div>
             <div className="system" style={{ color: mode ? "" : "#ffff" }}>
-              Web Design
+              App Web
             </div>
           </div>
 
@@ -534,6 +525,201 @@ const Body = () => {
                 className="web-app web-app-left"
                 src={require("../../image/harley-img4.png")}
                 onClick={() => ViewContentWeb(require("../../image/harley-img4.png"))}
+              />
+            </div>
+          </div>
+        </div>
+
+        <div
+          className="app-content"
+          style={{ backgroundColor: mode ? "" : "#454645" }}
+        >
+          <div id="desc-tenth-app" className="desc-app">
+            <div>
+              <div className="linguage" style={{ color: mode ? "" : "#ffff" }}>
+                ReactJs
+              </div>
+              <div className="project" style={{ color: mode ? "" : "#ffff" }}>
+                Spotify website
+              </div>
+              <div
+                className="desc-projeto"
+                style={{ color: mode ? "" : "#ffff" }}
+              >
+                Spotify é um serviço de streaming de música, podcast e vídeo que
+                foi lançado oficialmente em 7 de outubro de 2008. É o serviço de
+                streaming de música mais popular e usado do mundo. Ele é
+                desenvolvido pela startup Spotify AB em Estocolmo, Suécia.
+              </div>
+            </div>
+            <div className="system" style={{ color: mode ? "" : "#ffff" }}>
+              App Web
+            </div>
+          </div>
+
+          <div className="image-app">
+            <div style={{ display: "flex", flexDirection: "column" }}>
+              <img
+                className="web-app web-app-rigth"
+                src={require("../../image/spt-img1.png")}
+                onClick={() => ViewContentWeb(require("../../image/spt-img1.png"))}
+              />
+              <img
+                className="web-app web-app-left"
+                src={require("../../image/spt-img2.png")}
+                onClick={() => ViewContentWeb(require("../../image/spt-img2.png"))}
+              />
+              <img
+                className="web-app web-app-rigth"
+                src={require("../../image/spt-img3.png")}
+                onClick={() => ViewContentWeb(require("../../image/spt-img3.png"))}
+              />
+            </div>
+          </div>
+        </div>
+
+        <div
+          className="app-content"
+          style={{ backgroundColor: mode ? "" : "#454645" }}
+        >
+          <div id="desc-tenth-app" className="desc-app">
+            <div>
+              <div className="linguage" style={{ color: mode ? "" : "#ffff" }}>
+                ReactJs
+              </div>
+              <div className="project" style={{ color: mode ? "" : "#ffff" }}>
+                Amazônia website
+              </div>
+              <div
+                className="desc-projeto"
+                style={{ color: mode ? "" : "#ffff" }}
+              >
+                A floresta tropical amazônica, que cobre boa parte do noroeste do Brasil e se estende até
+                a Colômbia, o Peru e outros países da América do Sul, é a maior floresta tropical do mundo, famosa por sua biodiversidade.
+              </div>
+            </div>
+            <div className="system" style={{ color: mode ? "" : "#ffff" }}>
+              App Web
+            </div>
+          </div>
+
+          <div className="image-app">
+            <div style={{ display: "flex", flexDirection: "column" }}>
+              <img
+                className="web-app web-app-rigth"
+                src={require("../../image/projeto1.png")}
+                onClick={() => ViewContentWeb(require("../../image/projeto1.png"))}
+              />
+              <img
+                className="web-app web-app-left"
+                src={require("../../image/projeto3.png")}
+                onClick={() => ViewContentWeb(require("../../image/projeto3.png"))}
+              />
+              <img
+                className="web-app web-app-rigth"
+                src={require("../../image/projeto4.png")}
+                onClick={() => ViewContentWeb(require("../../image/projeto4.png"))}
+              />
+              <img
+                className="web-app web-app-rigth"
+                src={require("../../image/projeto5.png")}
+                onClick={() => ViewContentWeb(require("../../image/projeto5.png"))}
+              />
+            </div>
+          </div>
+        </div>
+
+        <div
+          className="app-content"
+          style={{ backgroundColor: mode ? "" : "#454645" }}
+        >
+          <div id="desc-tenth-app" className="desc-app">
+            <div>
+              <div className="linguage" style={{ color: mode ? "" : "#ffff" }}>
+                ReactJs | Nodejs
+              </div>
+              <div className="project" style={{ color: mode ? "" : "#ffff" }}>
+                Sistema de consultas médicas
+              </div>
+              <div
+                className="desc-projeto"
+                style={{ color: mode ? "" : "#ffff" }}
+              >
+                Sistema médico fullstack pessoal.
+              </div>
+            </div>
+            <div className="system" style={{ color: mode ? "" : "#ffff" }}>
+              App Web
+            </div>
+          </div>
+
+          <div className="image-app">
+            <div style={{ display: "flex", flexDirection: "column" }}>
+              <img
+                className="web-app web-app-rigth"
+                src={require("../../image/img1-medical.png")}
+                onClick={() => ViewContentWeb(require("../../image/img1-medical.png"))}
+              />
+              <img
+                className="web-app web-app-left"
+                src={require("../../image/img2-medical.png")}
+                onClick={() => ViewContentWeb(require("../../image/img2-medical.png"))}
+              />
+              <img
+                className="web-app web-app-rigth"
+                src={require("../../image/img3-medical.png")}
+                onClick={() => ViewContentWeb(require("../../image/img3-medical.png"))}
+              />
+              <img
+                className="web-app web-app-left"
+                src={require("../../image/img4-medical.png")}
+                onClick={() => ViewContentWeb(require("../../image/img4-medical.png"))}
+              />
+            </div>
+          </div>
+        </div>
+
+        <div
+          className="app-content"
+          style={{ backgroundColor: mode ? "" : "#454645" }}
+        >
+          <div id="desc-tenth-app" className="desc-app">
+            <div>
+              <div className="linguage" style={{ color: mode ? "" : "#ffff" }}>
+                ReactJs | Nodejs
+              </div>
+              <div className="project" style={{ color: mode ? "" : "#ffff" }}>
+                Puebla website
+              </div>
+              <div
+                className="desc-projeto"
+                style={{ color: mode ? "" : "#ffff" }}
+              >
+                Puebla, oficialmente Heroica Puebla de Zaragoza, é uma cidade e município
+                do México, capital do estado de Puebla. Tem cerca de 2,1 milhões de habitantes. Foi fundada em 1531 pelos espanhóis.
+              </div>
+            </div>
+            <div className="system" style={{ color: mode ? "" : "#ffff" }}>
+              App Web
+            </div>
+          </div>
+
+          <div className="image-app">
+            <div style={{ display: "flex", flexDirection: "column" }}>
+              <img
+                className="web-app web-app-rigth"
+                src={require("../../image/world1.png")}
+                onClick={() => ViewContentWeb(require("../../image/world1.png"))}
+              />
+              <img
+                className="web-app web-app-left"
+                src={require("../../image/world2.png")}
+                onClick={() => ViewContentWeb(require("../../image/world2.png"))}
+              />
+              <img
+                className="web-app web-app-rigth"
+                src={require("../../image/world3.png")}
+                onClick={() => ViewContentWeb(require("../../image/world3.png"))}
               />
             </div>
           </div>
